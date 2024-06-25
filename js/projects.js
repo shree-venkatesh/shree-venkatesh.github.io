01fb11b374e4481c3e0cb700728ec478580141c7
SV <vshree852@gmail.com>
2024-06-24 17:51:39 -0700

    add all projects

--- js/projects.js
+++ js/projects.js
@@ -1,12 +1,42 @@
 const container = document.querySelector('.project-page-metacontainer');
-const slides = document.querySelectorAll('.project-page-container');
+let slides = document.querySelectorAll('.active .project-page-container');
+
+// project page navigation buttons
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
+
+// nav button labels
 const left = document.getElementById('left');
 const right = document.getElementById('right');
+
+// project category selector buttons
+const tuasBtn = document.getElementById('tuas-button');
+const personalBtn = document.getElementById('personal-button');
+const academicBtn = document.getElementById('academic-button');
+
+// project category selector button labels
+const tuasH2 = document.getElementById('tuas-h2');
+const personalH2 = document.getElementById('personal-h2');
+const academicH2 = document.getElementById('academic-h2');
+
+// project gallery metacontainers
+const tuas = document.getElementById('tuas');
+const personal = document.getElementById('personal');
+const academic = document.getElementById('academic');
+
+// initialize which slide to show on initial render
 let currentSlide = 0;
+let currentCategory = "tuas";
 
 function updateButtons() {
+    // disable buttons if only one slide is present
+    if (slides.length === 1) {
+        left.classList.add("disable");
+        right.classList.add("disable");
+        return;
+    }
+
+    // disable left button, if on first slide
     prevBtn.disabled = currentSlide === 0;
     if (currentSlide === 0) {
         left.classList.add("disable");
@@ -14,6 +44,7 @@ function updateButtons() {
         left.classList.remove("disable");
     }
     
+    // disable right button, if on last slide
     nextBtn.disabled = currentSlide === slides.length - 1;
     if (currentSlide === slides.length - 1) {
         right.classList.add("disable");
@@ -23,8 +54,15 @@ function updateButtons() {
 }
 
 function scrollToSlide(index) {
-    const slide = document.getElementById(`${index}`);
+    const slide = document.getElementById(`${currentCategory}-${index}`);
     slide.scrollIntoView({ behavior: "smooth", block: "nearest" });
+    currentSlide = index;
+    updateButtons();
+}
+
+function initialScrollToSlide(index) {
+    const slide = document.getElementById(`${currentCategory}-${index}`);
+    slide.scrollIntoView();
     currentSlide = index;
     updateButtons();
 }
@@ -39,6 +77,49 @@ nextBtn.addEventListener('click', () => {
     if (currentSlide < slides.length - 1) {
         scrollToSlide(currentSlide + 1);
     }
+});
+
+// set the selected category as active, and the others to inactive
+tuasBtn.addEventListener('click', () => {
+    tuas.classList.add('active');
+    tuasH2.classList.add('selected');
+    personal.classList.remove('active');
+    personalH2.classList.remove('selected');
+    academic.classList.remove('active');
+    academicH2.classList.remove('selected');
+    slides = document.querySelectorAll('.active .project-page-container');
+    currentCategory = "tuas";
+    currentSlide = 0;
+    initialScrollToSlide(currentSlide)
+    updateButtons();
+});
+
+personalBtn.addEventListener('click', () => {
+    personal.classList.add('active');
+    personalH2.classList.add('selected');
+    tuas.classList.remove('active');
+    tuasH2.classList.remove('selected');
+    academic.classList.remove('active');
+    academicH2.classList.remove('selected');
+    slides = document.querySelectorAll('.active .project-page-container');
+    currentCategory = "personal";
+    currentSlide = 0;
+    initialScrollToSlide(currentSlide)
+    updateButtons();
+});
+
+academicBtn.addEventListener('click', () => {
+    academic.classList.add('active');
+    academicH2.classList.add('selected');
+    personal.classList.remove('active');
+    personalH2.classList.remove('selected');
+    tuas.classList.remove('active');
+    tuasH2.classList.remove('selected');
+    slides = document.querySelectorAll('.active .project-page-container');
+    currentCategory = "academic";
+    currentSlide = 0;
+    initialScrollToSlide(currentSlide)
+    updateButtons();
 });
 
 // Initial button state
